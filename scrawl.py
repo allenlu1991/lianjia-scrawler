614d187fd0cec86fa6e67f1c05ef98dddafa3198
allenlugit config --global user.email 903949263@qq.com <903949263@qq.com>
2018-04-07 22:08:17 +0800

    Initial commit

---
+++ scrawl.py
@@ -0,0 +1,20 @@
+﻿import core
+import model
+import settings
+
+def get_communitylist():
+	res = []
+	for community in model.Community.select():
+		res.append(community.title)
+	return res
+
+if __name__=="__main__":
+    # regionlist = settings.REGIONLIST # only pinyin support
+    regionlist = core.getRegionByArea(settings.AREA)
+    model.database_init()
+
+    # core.GetHouseByRegionlist(regionlist)
+    core.GetRentByRegionlist(regionlist)
+    # core.GetCommunityByRegionlist(regionlist) # Init,scrapy celllist and insert database; could run only 1st time
+    # # communitylist = get_communitylist() # Read celllist from database
+    # core.GetSellByCommunitylist(communitylist)
---
+++ settings.py
@@ -0,0 +1,12 @@
+DBENGINE = 'mysql' #ENGINE OPTIONS: mysql, sqlite3, postgresql
+DBNAME = 'fang'
+DBUSER = 'root'
+DBPASSWORD = '19901223'
+DBHOST = '127.0.0.1'
+DBPORT = 3306
+CITY = 'bj' # only one, shanghai=sh shenzhen=sh......
+# REGIONLIST = [u'junbo1',u'gongzhufen',u'wanshoulu1',u'wukesong1',u'guanganmen',u'liuliqiao1',u'taipingqiao1',u'dinghuisi'] # only pinyin support
+
+# REGIONLIST = [u'junbo1',u'gongzhufen',u'wanshoulu1'] # only pinyin support
+
+AREA = [u'chaoyang']
